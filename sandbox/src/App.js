3534a17040be79ad1b1738c394f7358f374d98ce
Jose <magicomiralles@gmail.com>
2020-09-02 00:16:10 -0400

    React forms state test.

--- sandbox/src/App.js
+++ sandbox/src/App.js
@@ -1,29 +1,79 @@
 import React from "react"
 
-class App extends React.Component{
+class App extends React.Component {
 
-    constructor(props){
-        super(props);
+    constructor() {
+        super();
         this.state = {
-            isLoggedIn: this.props.isLoggedIn
-        }
-        this.ButtonClicked = this.ButtonClicked.bind(this);
+            firstName: "Jose",
+            lastName: "Miralles",
+            isFriendly: true,
+            mainHand: "Right Handed"
+        };
+        this.HandleChange = this.HandleChange.bind(this);
+        this.onCheckboxClick = this.onCheckboxClick.bind(this);
+        this.onRadioChanged = this.onRadioChanged.bind(this);
     }
 
-    render(){
+    render() {
         return (
-        <span>
-            <h1>{this.state.isLoggedIn ? "You are logged in!" : "You need to log in!"}</h1>
-        <button onClick={this.ButtonClicked}>{this.state.isLoggedIn ? "Log out" : "Log in"}</button>
-        </span>
+            <span>
+                <input name="firstName" value={this.state.firstName} type="text" placeholder="First Name" onChange={this.HandleChange} />
+                <br />
+                <input name="lastName" value={this.state.lastName} type="text" placeholder="Last Name" onChange={this.HandleChange} />
+                <br />
+
+                <label>
+                <input name="isFriendly" type="checkbox" checked={this.state.isFriendly} onChange={this.onCheckboxClick} />
+                Friendly
+                </label>
+                <br/>
+
+                <label>
+                    <input
+                    type="radio"
+                    name="mainHand"
+                    value="Left Handed"
+                    checked={this.state.mainHand === "Left Handed"}
+                    onChange={this.onRadioChanged}/>Left Handed
+                </label>
+                <label>
+                    <input
+                    type="radio"
+                    name="mainHand"
+                    value="Right Handed"
+                    checked={this.state.mainHand === "Right Handed"}
+                    onChange={this.onRadioChanged}/> Right Handed
+                </label>
+
+                <hr />
+                <h3>Stats:</h3>
+                <p>{this.state.firstName} {this.state.lastName}</p>
+                <p>{this.state.isFriendly ? "Friendly" : "Not friendly"}</p>
+                <p>{this.state.mainHand}</p>
+            </span>
         );
     }
 
-    ButtonClicked(){
-        this.setState((prev) => {
-            return ({isLoggedIn: !prev.isLoggedIn})
+    HandleChange(event) {
+        const { name, value } = event.target;
+        this.setState({
+            [name]: value
         });
     }
+
+    onCheckboxClick(event) {
+        this.setState({
+            [event.target.name]: event.target.checked
+        });
+    }
+
+    onRadioChanged(event){
+        this.setState({
+            [event.target.name]: event.target.value
+        });
+    }
+
 }
 
 export default App;
